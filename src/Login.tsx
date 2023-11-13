d48c45d102b274ddd4eab9e20b4a6b976408ef0a
darraakk <aradbassirat18@gmail.com>
2023-11-13 23:17:44 +0330

    imp login

---
+++ src/Login.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('https://arad-test-app.aradbassirat18.workers.dev/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setMessage('Login successful!');
+      } else {
+        setMessage(`Login failed: ${data.message}`);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setMessage('An error occurred during login.');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Login;
